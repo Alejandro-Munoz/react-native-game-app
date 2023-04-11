d47a819d428c01998e6ba1d25641de75f2339028
Alejandro Munoz <alejandro.z.munoz@gmail.com>
2023-04-11 07:27:25 -0700

    total guess rounds

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -29,7 +29,7 @@ function GameScreen({ userNumber, onGameOver }) {
 
   useEffect(() => {
     if (currentGuess === userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
 
@@ -87,21 +87,21 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      <View>
-        {guessRounds.map((guessRound) => (
+      <View style={styles.listContainer}>
+        {/* {guessRounds.map((guessRound) => (
           <Text key={guessRound}>{guessRound}</Text>
-        ))}
+        ))} */}
+        <FlatList
+          data={guessRounds}
+          renderItem={(itemData) => (
+            <GuessLogItem
+              roundNumber={guessRoundsListLength - itemData.index}
+              guess={itemData.item}
+            />
+          )}
+          keyExtractor={(item) => item}
+        ></FlatList>
       </View>
-      <FlatList
-        data={guessRounds}
-        renderItem={(itemData) => (
-          <GuessLogItem
-            roundNumber={guessRoundsListLength - itemData.index}
-            guess={itemData.item}
-          />
-        )}
-        keyExtractor={(item) => item}
-      ></FlatList>
     </View>
   );
 }
@@ -122,4 +122,9 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flex: 1,
   },
+  listContainer: {
+    flex: 1,
+    padding: 16,
+
+  }
 });
